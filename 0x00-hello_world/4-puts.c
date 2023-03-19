ce71c36c3e897c750069af2b5f73d4901493346e
Jerome-kimosop <kapkorkimosop10@gmail.com>
2023-03-19 07:12:17 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  *main - entry point
- *return - returns 0
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main-Entry poin
- *return- returns 0
+ *main- Entry poin
+ *Return: Always 0 (Success)
  */
 int main(void)
 {

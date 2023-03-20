afe5c1b38ca2ffaf9bc511fc4208453e372321c4
Jerome-kimosop <kapkorkimosop10@gmail.com>
2023-03-20 16:13:10 -0700

    Numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,13 @@
 int main(void)
 {
 	int i, j;
-for (i = 0; i < 10; i++)
-{
-	for (j = i; j < 10; j++)
+
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
-		putchar(j + '0');
+		putchar(' ');
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }

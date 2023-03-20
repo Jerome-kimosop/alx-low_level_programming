3e84ba75fe76126b99d3d1ab4680ee5d3ab1d364
Jerome-kimosop <kapkorkimosop10@gmail.com>
2023-03-20 12:36:51 -0700

    Alphabet exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,18 @@
 /**
  *main- Entry point
  *Return: Always 0 (Success)
- int main()
- {
- char let = 'a';
- while (let <= 'z')
- {
- if (let != 'q' && let != 'e')
- {
- putchar(let);
- let++;
- }
- putchar('\n');
- return( 0);
+ */
+int main(void)
+{
+	char let = 'a';
+while (let <= 'z')
+{
+if (let != 'q' && let != 'e')
+{
+	putchar(let);
+	let++;
+}
+putchar('\n');
+return (0);
+}
 }

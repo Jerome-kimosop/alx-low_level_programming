182880385290c2d6f84da923feb0e38142662694
Jerome-kimosop <kapkorkimosop10@gmail.com>
2023-03-22 11:03:51 -0700

    functions,nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,13 +3,9 @@
 
 #include <stdio.h>
 
-int main(void);
-
-int function1(int arg1, char arg2);
-
-void function2(void);
-
 /* Declaration of _putchar function */
 int _putchar(char c);
+/* Declaration of print_alphabet function */
+void print_alphabet(void);
 
 #endif /* MAIN_H */

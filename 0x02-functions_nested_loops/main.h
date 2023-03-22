3fe7f6ff9b8fbe5abb02e2c6f3d33486ab422eb2
Jerome-kimosop <kapkorkimosop10@gmail.com>
2023-03-22 09:48:14 -0700

    prototypes for main.h and putchar function

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int main(void);
+
+int function1(int arg1, char arg2);
+
+void function2(void);
+
+/* Declaration of _putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */

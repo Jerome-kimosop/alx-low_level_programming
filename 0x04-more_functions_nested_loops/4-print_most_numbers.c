663c6a29089d684f0569bf9d7bb730f48ed2864b
Jerome-kimosop <kapkorkimosop10@gmail.com>
2023-03-24 19:51:38 -0700

    more functions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++) {
-		if (i == 2 || i == 4) {
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
 			continue;
 		}
 		_putchar(i + '0');
